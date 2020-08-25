c4331c33d59b640ab5a577f6d69627c750ab7a92
David Daza <03dazal@gmail.com>
2020-08-25 15:35:19 -0500

    Setup y layout inicial

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        ibm: ['"IBM Plex Sans"', 'sans-serif'],
+        chango: ['Chango', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+      colors: {
+        purple: '#4f2fef',
+        charcoal: '#293845',
+        violet: '#f282fc',
+        'barbie-pink': '#da1c95',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
